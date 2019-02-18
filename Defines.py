b4e3407941d0213398ce0ebf8c18d12753a7bc6c
BlickGin <aurelien.laurent.1@etsmtl.net>
2019-02-18 12:00:17 -0500

    Dev Gui

--- Defines.py
+++ Defines.py
@@ -1,5 +1,3 @@
-
-
 ########################################################################################################################
 # Types d'activations
 
@@ -10,3 +8,11 @@ ACTI_TYPE_STEP = 3
 ACTI_TYPE_RAMP = 4
 ACTI_TYPE_RELU = 5
 ACTI_TYPE_GAUSS = 6
+
+acti_dict = {'ACTI_TYPE_SIGMOID': 0,
+             'ACTI_TYPE_TANH': 1,
+             'ACTI_TYPE_SIN': 2,
+             'ACTI_TYPE_STEP': 3,
+             'ACTI_TYPE_RAMP': 4,
+             'ACTI_TYPE_RELU': 5,
+             'ACTI_TYPE_GAUSS': 6}
--- Gui.py
+++ Gui.py
@@ -1,4 +1,15 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.use("TkAgg")
 import tkinter as tk
+from tkinter import messagebox
+
+import numpy as np
+import matplotlib.backends.tkagg as tkagg
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib import figure
+from matplotlib import axes
 import pygubu
 import pickle
 from save_load import *
@@ -23,96 +34,224 @@ class Dialog:
         self.top.destroy()
 
 
+def draw_graph(frame, error_values):
+    fig = plt.figure(1)
+    plt.ion()
+    t = list(range(0, len(error_values)))
+    s = error_values
+    plt.plot(t, s)
+
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    plot_widget = canvas.get_tk_widget()
+
+    plot_widget.grid(row=0, column=0)
+    tk.mainloop()
+
+
 class Application:
     def __init__(self, master):
         self.nb_of_networks = 0
-        self.master = master
         self.builder = builder = pygubu.Builder()
         builder.add_from_file('Gui.ui')
+
+        self.builder2 = builder2 = pygubu.Builder()
+        self.builder2.add_from_file('Gui.ui')
+
         self.mainwindow = builder.get_object('Toplevel_2', master)
-        self.dict_of_networks = {}
-        self.dict_of_networks_name = 'Network_List.pkl'
+        builder.connect_callbacks(self)
+        master.protocol("WM_DELETE_WINDOW", self.on_close_window)
+
+        # Gère la sélection d'un item dans la liste
+        def curselect(event):
+            widget = event.widget
+            selection = widget.curselection()
+            picked = widget.get(selection[0])
+            net = load(picked)
+            txt = "Nom : " + net.name + "\n\r" \
+                                        "Nombre de couches : " + str(net.nb_couches) + "\n\r" \
+                                                                                       "Config [Entrées, C1, C2, ..., Cn, Sorties] : " + str(
+                net.nb_unit_par_couche) + " \n\r" \
+                                          "Type d'activation : " + str(net.acti_type)
+
+            self.netinfos_label.config(text=txt)
+            print(net.nb_unit_par_couche)
+            draw_graph(frame_13, net.error_count)
+
+        self.networks_listbox = net_lbox = builder.get_object('Network_list')
+        self.networks_listbox.bind('<<ListboxSelect>>', curselect)
+
+        self.netinfos_label = net_infos = builder.get_object('Network_Infos')
+
+        # self.graph_win = graph_win = builder.get_object('Graphics')
+        # graph_win.pack()
+
+        self.frame_13 = frame_13 = builder.get_object('Frame_13')
+
+        self.list_of_networks = {}
+        self.list_of_networks_name = 'Network_List.pkl'
         builder.connect_callbacks(self)
 
-        load(self.dict_of_networks_name)
+        self.list_of_networks = load(self.list_of_networks_name)
+        self.nb_of_networks = len(self.list_of_networks)
+
+        net_lbox.select_clear(tk.END)
+        for i in self.list_of_networks:
+            text = i
+            net_lbox.insert(tk.END, text)
+        net_lbox.see(tk.END)
+        net_lbox.selection_set(tk.END)
+
+    def on_close_window(self, event=None):
+        print("closed")
+        self.mainwindow.master.destroy()
+        exit()
 
     def on_new_network_click(self):
-        val = None
-        while type(val) is not int:
-            d = Dialog(root, "Nombre de couches")
-            root.wait_window(d.top)
+        top3 = tk.Toplevel(self.mainwindow)
+
+        frame3 = self.builder2.get_object('New_Network_Window', top3)
+        self.builder2.connect_callbacks(self)
+
+    def on_ok_click(self):
+        good_data = False
+
+        name_input = 0
+        nb_layers = 0
+        nb_unit_per_layer = 0
+        nb_input = 0
+        nb_output = 0
+
+        name_input_entry = self.builder2.get_object('Network_Name_Entry')
+        nb_layers_entry = self.builder2.get_object('Number_Of_Layer_Entry')
+        nb_unit_per_layer_entry = self.builder2.get_object('Number_Of_Unit_Entry')
+        nb_input_entry = self.builder2.get_object('Number_Of_Inputs_Entry')
+        nb_output_entry = self.builder2.get_object('Number_Of_Outputs_Entry')
+        acti_type_entry = self.builder2.get_object('Acti_Type_Menu_Button')
+        name_input = name_input_entry.get()
+        nb_layers = nb_layers_entry.get()
+        nb_unit_per_layer = nb_unit_per_layer_entry.get()
+        nb_input = nb_input_entry.get()
+        nb_output = nb_output_entry.get()
+        acti_type = acti_type_entry.selection_get()
+        print(acti_type)
+
+        nb_unit_per_layer = nb_unit_per_layer.split(",")
+
+        try:
+            nb_layers = int(nb_layers)
             try:
-                val = int(d.return_value)
-            except ValueError:
-                val = None
-
-        nb_couches = int(val)
-        nb_unit_par_couche = []
-        val = None
-        for i in range(nb_couches + 1):
-            if i == 0:
-                while type(val) is not int:
-                    d = Dialog(root, "Nombre d'entrées")
-                    root.wait_window(d.top)
+                for i in nb_unit_per_layer:
+                    i = int(i)
+                try:
+                    nb_input = int(nb_input)
                     try:
-                        val = int(d.return_value)
+                        nb_output = int(nb_output)
+                        good_data = True
                     except ValueError:
-                        val = None
-                nb_unit_par_couche.append(val)
-                val = None
+                        messagebox.showwarning("Wrong Type Entry", "Please enter an integer for the number of outputs")
+                except ValueError:
+                    messagebox.showwarning("Wrong Type Entry", "Please enter an integer for the number of inputs")
+            except ValueError:
+                messagebox.showwarning("Wrong Type Entry",
+                                       "Please enter only integers for the number of units per layers")
+        except ValueError:
+            messagebox.showwarning("Wrong Type Entry", "Please enter an integer for the number of layers")
+
+        if good_data is True:
+            if nb_layers is not len(nb_unit_per_layer):
+                messagebox.showwarning("Wrong Type Entry",
+                                       "Please enter exactly " + str(nb_layers) + " layers in number of layers")
+            elif name_input is "":
+                messagebox.showwarning("Wrong Type Entry", "Please enter a name")
             else:
-                if i is nb_couches:
-                    txt = "Nombre d'unités à la couche " + str(i) + " (Sortie)"
-                    while type(val) is not int:
-                        d = Dialog(root, txt)
-                        root.wait_window(d.top)
-                        try:
-                            val = int(d.return_value)
-                        except ValueError:
-                            val = None
-
-                    nb_unit_par_couche.append(val)
-                    val = None
-                else:
-                    txt = "Nombre d'unités à la couche " + str(i)
-                    while type(val) is not int:
-                        d = Dialog(root, txt)
-                        root.wait_window(d.top)
-                        try:
-                            val = int(d.return_value)
-                        except ValueError:
-                            val = None
-                    nb_unit_par_couche.append(val)
-                    val = None
-
-        d = Dialog(root, "Activation Type (Enter number between 1 and 7) \n"
-                         "0 : SIGMOID\n"
-                         "1 : TANH\n"
-                         "2 : SIN\n"
-                         "3 : STEP\n"
-                         "4 : RAMP\n"
-                         "5 : RELU\n"
-                         "6 : GAUSS")
-        root.wait_window(d.top)
-        acti = d.return_value
-
-        net = Network()
-        net.nb_unit_par_couche = nb_unit_par_couche
-        net.nb_couches = nb_couches
-        net.acti_type = acti
-
-        net.init()
-        self.nb_of_networks += 1
-        fname = "Network_" + str(self.nb_of_networks) + ".pkl"
-        self.dict_of_networks[fname] = net
-        save(net, fname)
-
-
+                print("OK")
+    # def on_new_network_click(self):
+    #     d = Dialog(root, "Nom")
+    #     root.wait_window(d.top)
+    #     name = d.return_value
+    #     val = None
+    #     while type(val) is not int:
+    #         d = Dialog(root, "Nombre de couches")
+    #         root.wait_window(d.top)
+    #         try:
+    #             val = int(d.return_value)
+    #         except ValueError:
+    #             val = None
+    #
+    #     nb_couches = int(val)
+    #     nb_unit_par_couche = []
+    #     val = None
+    #     for i in range(nb_couches + 1):
+    #         if i == 0:
+    #             while type(val) is not int:
+    #                 d = Dialog(root, "Nombre d'entrées")
+    #                 root.wait_window(d.top)
+    #                 try:
+    #                     val = int(d.return_value)
+    #                 except ValueError:
+    #                     val = None
+    #             nb_unit_par_couche.append(val)
+    #             val = None
+    #         else:
+    #             if i is nb_couches:
+    #                 txt = "Nombre d'unités à la couche " + str(i) + " (Sortie)"
+    #                 while type(val) is not int:
+    #                     d = Dialog(root, txt)
+    #                     root.wait_window(d.top)
+    #                     try:
+    #                         val = int(d.return_value)
+    #                     except ValueError:
+    #                         val = None
+    #
+    #                 nb_unit_par_couche.append(val)
+    #                 val = None
+    #             else:
+    #                 txt = "Nombre d'unités à la couche " + str(i)
+    #                 while type(val) is not int:
+    #                     d = Dialog(root, txt)
+    #                     root.wait_window(d.top)
+    #                     try:
+    #                         val = int(d.return_value)
+    #                     except ValueError:
+    #                         val = None
+    #                 nb_unit_par_couche.append(val)
+    #                 val = None
+    #
+    #     d = Dialog(root, "Activation Type (Enter number between 1 and 7) \n"
+    #                      "0 : SIGMOID\n"
+    #                      "1 : TANH\n"
+    #                      "2 : SIN\n"
+    #                      "3 : STEP\n"
+    #                      "4 : RAMP\n"
+    #                      "5 : RELU\n"
+    #                      "6 : GAUSS")
+    #     root.wait_window(d.top)
+    #     acti = d.return_value
+    #
+    #     net = Network()
+    #     net.nb_unit_par_couche = nb_unit_par_couche
+    #     net.nb_couches = nb_couches
+    #     net.acti_type = acti
+    #     net.name = name
+    #
+    #     net.init()
+    #     self.nb_of_networks += 1
+    #     fname = "Network_" + str(self.nb_of_networks) + ".pkl"
+    #     self.list_of_networks += [fname]
+    #     save(net, fname)
+    #     save(self.list_of_networks, self.list_of_networks_name)
+    #
+    #     self.networks_listbox.delete(0, tk.END)
+    #     self.networks_listbox.select_clear(tk.END)
+    #     for i in range(self.nb_of_networks):
+    #         text = self.list_of_networks[i]
+    #         self.networks_listbox.insert(tk.END, text)
+    #     self.networks_listbox.see(tk.END)
+    #     self.networks_listbox.selection_set(tk.END)
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     app = Application(root)
-
     root.mainloop()
 
