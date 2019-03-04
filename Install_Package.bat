3520c4f913b3a70523c36a488729e8fa9c8c745e
apaudette <andre-philippe.audette.1@ens.etsmtl.ca>
2019-03-04 12:14:45 -0500

    Add files via upload

---
+++ Install_Package.bat
@@ -0,0 +1,7 @@
+python -m pip install -U matplotlib --user
+pip install numpy --user
+python -m pip install -U pygubu --user
+python -m pip install -U Tkinter --user
+python -c -m tkinter | import
+python -c tkinter.test()
+pause
